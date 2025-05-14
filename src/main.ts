7b1eb5a838a7fd4a4e6f93541de41fdc84ecf188
Jason Clarke <jasondanielclarke@gmail.com>
2025-05-14 08:22:51 +0100

    Add a data point at sanfrancisco airport

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import './main.css'
-import { Viewer, Ion, Terrain, createOsmBuildingsAsync } from "cesium";
+import { 
+  Viewer, 
+  Ion, 
+  Terrain, 
+  createOsmBuildingsAsync,
+  Cartesian3,
+  Color
+} from "cesium";
 import "cesium/Build/Cesium/Widgets/widgets.css";
 
 const env = await import.meta.env
@@ -13,4 +20,15 @@ const viewer = new Viewer('cesiumContainer', {
 });
 
 const osmBuildings = await createOsmBuildingsAsync();
-viewer.scene.primitives.add(osmBuildings);;
+viewer.scene.primitives.add(osmBuildings);
+
+// This is one of the first radar samples collected for our flight.
+const dataPoint = { longitude: -122.38985, latitude: 37.61864, height: -27.32 };
+// Mark this location with a red point.
+const pointEntity = viewer.entities.add({
+  description: `First data point at (${dataPoint.longitude}, ${dataPoint.latitude})`,
+  position: Cartesian3.fromDegrees(dataPoint.longitude, dataPoint.latitude, dataPoint.height),
+  point: { pixelSize: 10, color: Color.RED }
+});
+// Fly the camera to this point.
+viewer.flyTo(pointEntity);
